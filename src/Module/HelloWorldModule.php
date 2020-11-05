5e1dc9324bd8410292f39d6b6f0571b1b1588bfa
Himalaya <kim.mayr@gmail.com>
2020-11-05 21:49:37 +0100

    Use Haste to generate HelloWorldModule wildcard

--- src/Module/HelloWorldModule.php
+++ src/Module/HelloWorldModule.php
@@ -2,41 +2,15 @@
 
 namespace Himalaya\ContaoHelloWorldBundle\Module;
 
-use Contao\Module;
-use Contao\BackendTemplate;
-
-use Patchwork\Utf8;
-
+use Haste\Frontend\AbstractFrontendModule;
 use Himalaya\ContaoHelloWorldBundle\Library\MessageGenerator;
 
-class HelloWorldModule extends Module
+class HelloWorldModule extends AbstractFrontendModule
 {
     /**
      * @var string
      */
     protected $strTemplate = 'mod_helloWorld';
-
-    /**
-     * Displays a wildcard in the back end.
-     *
-     * @return string
-     */
-    public function generate()
-    {
-        if (TL_MODE == 'BE') {
-            $template = new BackendTemplate('be_wildcard');
-
-            $template->wildcard = '### ' . Utf8::strtoupper($GLOBALS['TL_LANG']['FMD']['helloWorld'][0]) . ' ###';
-            $template->title = $this->headline;
-            $template->id = $this->id;
-            $template->link = $this->name;
-            $template->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
-
-            return $template->parse();
-        }
-
-        return parent::generate();
-    }
 
     /**
      * @inheritdoc
